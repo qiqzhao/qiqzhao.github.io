37505b0cdc53028667cad8258fd062ffc3c376dc
qiqzhao <bme_ritter@foxmail.com>
2024-08-27 10:23:40 +0800

    feat: about

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -8,7 +8,7 @@ import SectionTitle from "@/components/about/section-title";
 
 const About: FC = () => {
   return (
-    <section className="mx-auto px-5 mt-16 lg:px-48">
+    <section className="mx-auto px-5 mt-16 lg:px-48 mb-8">
       <Header />
 
       <div className="flex flex-col items-center">
@@ -21,7 +21,7 @@ const About: FC = () => {
         />
         <h1 className="mt-6 text-lg font-bold">{`qiqzhao's Blog`}</h1>
         <a
-          className="text-sm leading-8 text-gray-400"
+          className="text-sm leading-8 text-gray-400 hover:text-blue-500 "
           href="https://github.com/qiqzhao"
           target="__blank"
         >
@@ -29,9 +29,33 @@ const About: FC = () => {
         </a>
       </div>
 
+      <div>
+        <SectionTitle>Nice to meet you</SectionTitle>
+        <p className="text-sm leading-8 text-gray-400">
+          爱画画的码农，偶尔在小红书发的那些 &nbsp;
+          <a
+            className="text-blue-500 hover:text-blue-700 "
+            href="https://www.xiaohongshu.com/user/profile/5af462ef4eacab71f9c249aa?xhsshare=CopyLink&appuid=5af462ef4eacab71f9c249aa&apptime=1724724646&share_id=b6485cb0ac0543eb991d576853a4b85d"
+          >
+            画
+          </a>
+        </p>
+      </div>
+
       <div className="flex flex-col gap-4 text-gray-400 leading-10">
         <div>
           <SectionTitle>折腾过的</SectionTitle>
+
+          <Li>
+            仿网易云的，
+            <a
+              className="text-blue-500 hover:text-blue-700 "
+              href="https://qiqzhao.github.io/js-demo/song-scroll/"
+              target="__blank"
+            >
+              歌词滚动效果
+            </a>
+          </Li>
 
           <Li>
             目前正在做的 React UI 库，
@@ -46,10 +70,7 @@ const About: FC = () => {
 
           <Li>
             想做一个教背古诗的小程序，但苦于买不起服务器，暂时搁置，没有link，小程序搜索，
-            <a
-              className="text-blue-500 hover:text-blue-700 "
-              href="#"
-            >
+            <a className="text-blue-500 hover:text-blue-700 " href="#">
               个人学背古诗
             </a>
           </Li>
@@ -97,6 +118,8 @@ const About: FC = () => {
           <Li>Webpack </Li>
           <Li>MySQL </Li>
           <Li>MongoDB </Li>
+          <Li>Next.js </Li>
+          <Li>Tailwind </Li>
         </div>
 
         <div>
